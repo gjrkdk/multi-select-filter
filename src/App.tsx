faebe375d4c83ab5be0ab2b3f207e29ada6b3dd3
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-06-12 16:05:11 +0200

    use tailwind in App.tsx for testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,11 @@
-export const App = () => {
-  return <div>App</div>;
-};
+function App() {
+  return (
+    <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm text-center">
+      <h1 className="text-2xl font-bold text-blue-500">
+        Bol Multi-Select Filter
+      </h1>
+    </div>
+  );
+}
+
+export default App;
