f801e4c342c3d88e6fdc8c0e488ea4cee14b68fd
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-06-14 14:16:41 +0200

    add test for app component

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,15 @@
 import { render } from "@testing-library/react";
 import { screen } from "@testing-library/dom";
-import App from "./App";
+import { MockedProvider } from "@apollo/client/testing";
+import { App } from "./App";
 
 describe("Simple working test", () => {
   it("renders the app with the correct title", () => {
-    render(<App />);
+    render(
+      <MockedProvider>
+        <App />
+      </MockedProvider>
+    );
     const titleElement = screen.getByText(/Bol Multi-Select Filter/i);
     expect(titleElement).toBeInTheDocument();
   });
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,10 @@
-import { useQuery } from "@apollo/client";
-import type { GetItemsQuery } from "./generated/graphql";
-import { GET_ITEMS } from "./graphql/queries/getItems";
+import { ItemList } from "./components/Itemlist";
 
-function App() {
-  const { loading, error, data } = useQuery<GetItemsQuery>(GET_ITEMS);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
-
+export const App = () => {
   return (
-    <ul className="mt-4 space-y-2">
-      {data?.items.map((item: string, index: number) => (
-        <li key={index} className="text-gray-700">
-          {item}
-        </li>
-      ))}
-    </ul>
+    <div>
+      <h1 className="text-2xl font-bold">Bol Multi-Select Filter</h1>
+      <ItemList />
+    </div>
   );
-}
-
-export default App;
+};
