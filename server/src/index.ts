7e8a73f8843d6779d26eaf372e2ea486887a2955
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-06-15 14:26:13 +0200

    configure server port using environment variable or default to 4000

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,14 @@
+import "dotenv/config";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs, resolvers } from "./graphql";
 
+const port = process.env.PORT ? parseInt(process.env.PORT) : 4000;
+
 async function startApolloServer() {
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 }
+    listen: { port }
   });
   console.log(`Server ready at: ${url}graphql`);
 }
