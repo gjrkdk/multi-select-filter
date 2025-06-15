f4434b048e213d52db5b7049fd0277a5c554af4d
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-06-15 10:37:10 +0200

    remove test app.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,6 @@ import { ItemList } from "./components/ItemList/ItemList";
 export const App = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold">Bol Multi-Select Filter</h1>
       <ItemList />
     </div>
   );
