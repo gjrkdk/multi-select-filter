e1e6eeb4553027e3936de01f45687a134dfe3632
gjrkdk <89792219+GJRKZB@users.noreply.github.com>
2025-06-17 22:03:03 +0200

    simplify itemrow component structure and improve accessibility

--- client/src/components/MultiSelectFilter/ItemRow/ItemRow.tsx
+++ client/src/components/MultiSelectFilter/ItemRow/ItemRow.tsx
@@ -7,20 +7,22 @@ interface ItemRowProps {
 }
 
 export const ItemRow = ({ item, isSelected, toggle }: ItemRowProps) => (
-  <li className="flex items-center space-x-2">
-    <label className="flex items-center space-x-2 cursor-pointer select-none">
-      <input
-        name={`item-checkbox-${item}`}
-        type="checkbox"
-        checked={isSelected}
-        onChange={() => toggle(item)}
-        className="accent-blue-500 w-4 h-4 border border-[#D2D1CD]"
-      />
-      <span
-        className={`text-sm ${isSelected ? "text-[#3063CF]" : "text-gray-700"}`}
-      >
-        {decodeHtml(item)}
-      </span>
+  <li className="flex items-center space-x-2" role="none">
+    <input
+      id={`item-${item}`}
+      name={`item-checkbox-${item}`}
+      type="checkbox"
+      checked={isSelected}
+      onChange={() => toggle(item)}
+      className="accent-blue-500 w-4 h-4 border border-[#D2D1CD]"
+    />
+    <label
+      htmlFor={`item-${item}`}
+      className={`text-sm cursor-pointer select-none ${
+        isSelected ? "text-[#3063CF]" : "text-gray-700"
+      }`}
+    >
+      {decodeHtml(item)}
     </label>
   </li>
 );
